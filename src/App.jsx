230e9979803c60fe644754edfe8f5db4fa39513b
Natan Lima <99678150+NatanALima@users.noreply.github.com>
2023-07-28 00:12:26 -0300

    Estiliza a tela principal (component App)

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,34 @@
+import { createGlobalStyle, styled } from "styled-components"
 import InputContainer from "./components/layout/InputContainer"
 function App() {
+  const GlobalStyle = createGlobalStyle `
+    body {
+      background-color: #1A1B26;
+      color: white;
+      font-family: 'Ubuntu Mono', monospace;
+    }
+  `
 
+  const MainContainer = styled.main`
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin-top: 3em;
+  `;
+
+  const MainTitle = styled.h1`
+    font-size: 3em;
+  `
   return (
-    <main>
-      <h1>To do List</h1>
-      <InputContainer type={"text"} placeholder={"Adicione um novo Texto"} btnSelect={"add"} />
-      <InputContainer type={"text"} disabled={true} btnSelect={"collection"} btnCollection={"default"}/>
-    </main>
+    <>
+      <GlobalStyle/>
+      <MainContainer>
+        <MainTitle>To Do List</MainTitle>
+        <InputContainer type={"text"} placeholder={"Adicione um novo Texto"} btnSelect={"add"} />
+        <InputContainer type={"text"} disabled={true} btnSelect={"collection"} btnCollection={"default"}/>
+      </MainContainer>
+    </>
   )
 }
 
